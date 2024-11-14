b8a4a778aec39a4fae1103570935c3df778d9fb3
James Wallington <jameswallington11@gmail.com>
2024-11-14 15:34:28 +0000

    fix/getall games

--- server/controllers/gamesController.js
+++ server/controllers/gamesController.js
@@ -10,25 +10,7 @@ const themes = JSON.parse(fs.readFileSync("./data/themes.json"));
 const platforms = JSON.parse(fs.readFileSync("./data/platforms.json"));
 
 export const getAllGames = (req, res) => {
-  try {
-    console.log(req.query);
-
-    if (req.query.themeId) {
-      const filteredGames = games.filter((game) => {
-        const someGames = game.themes?.some((theme) => {
-          return theme.id === Number(req.query.themeId);
-        });
-
-        return someGames;
-      });
-
-      return res.json({ count: filteredGames.length, games: filteredGames });
-    }
-
-    return res.json({ count: games.length, games: games });
-  } catch (error) {
-    console.error(error);
-  }
+  return res.json(games);
 };
 
 export const getGenres = (req, res) => {
@@ -80,25 +62,45 @@ export const getGamesById = (req, res) => {
 
 export const getGamesByTheme = (req, res) => {
   try {
-    console.log("Hello");
-    const themeId = req.params.themeId;
-    console.log(themeId);
-    const matchingGames = games.filter((game) => {
-      console.log(game.themes);
-      return game.themes.id.includes(themeId);
-    });
-    // const matchingGames = games.filter((game) => game.theme === theme);
+    console.log(req.query);
 
-    if (matchingGames.length > 0) {
-      console.log(matchingGames);
+    if (req.query.themeId) {
+      const filteredGames = games.filter((game) => {
+        const someGames = game.themes?.some((theme) => {
+          return theme.id === Number(req.query.themeId);
+        });
 
-      return res.json(matchingGames);
-    } else {
-      return res.status(404).json({ msg: "Games not found" });
+        return someGames;
+      });
+
+      return res.json({ count: filteredGames.length, games: filteredGames });
     }
+
+    return res.json({ count: games.length, games: games });
   } catch (error) {
     console.error(error);
   }
+
+  // try {
+  //   console.log("Hello");
+  //   const themeId = req.params.themeId;
+  //   console.log(themeId);
+  //   const matchingGames = games.filter((game) => {
+  //     console.log(game.themes);
+  //     return game.themes.id.includes(themeId);
+  //   });
+  //   // const matchingGames = games.filter((game) => game.theme === theme);
+
+  //   if (matchingGames.length > 0) {
+  //     console.log(matchingGames);
+
+  //     return res.json(matchingGames);
+  //   } else {
+  //     return res.status(404).json({ msg: "Games not found" });
+  //   }
+  // } catch (error) {
+  //   console.error(error);
+  // }
 };
 
 // export const getGamesBy = (req, res) => {
