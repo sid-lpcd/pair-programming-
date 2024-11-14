af9278224b4a2ad7a238ff6e93bd185d8e5d6b63
James Wallington <jameswallington11@gmail.com>
2024-11-14 10:43:52 +0000

    fix/server import

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { gamesRoutes } from "./routes/gamesRoutes.js";
+import gamesRoutes from "./routes/gamesRoutes.js";
 
 const app = express();
 
